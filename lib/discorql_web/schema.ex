9d45acaed51b8f971b3bfd12b723ad2cfebe8846
Joshua Kidd <kidd.josh.343@gmail.com>
2018-11-02 00:19:23 -0500

    added some more fields to all the types for channel, user, messages

--- lib/discorql_web/schema.ex
+++ lib/discorql_web/schema.ex
@@ -4,14 +4,76 @@ defmodule DiscorqlWeb.Schema do
   alias Nostrum.Api
   alias Nostrum.Struct.Snowflake
 
+  object :channel do
+    field :id, :snowflake
+    field :guild_id, :snowflake
+    field :position, :integer
+    field :name, :string
+    field :topic, :string
+    field :nsfw, :boolean
+  end
+
   object :message do
-    field :id, :string
+    field :id, :snowflake
+    field :guild_id, :snowflake
+    field :channel_id, :snowflake
     field :content, :string
+    field :pinned, :boolean
+    field :author, :user
+    field :mentions, list_of(:user)
+    field :mention_everyone, :boolean
+    field :mention_roles, list_of(:snowflake)
+
+    # TODO: Turn this into a built-in DateTime type or something.
+    # actually I don't think this is necessary unless doing server side stuff.
+    field :timestamp, :string
+
+    # TODO: Figure out how to resolve this... maybe it has to be done when the query happens.
+    # field :channel, :channel
+    # field :guild, :guild
+  end
+
+  object :user do
+    field :id, :snowflake
+    field :username, :string
+    field :discriminator, :string
+    field :avatar, :string
+    field :bot, :boolean
+    field :verified, :boolean
+  end
+
+  object :embed do
+  end
+
+  # TODO: move these test resolvers to their own files/folders under web/resolvers
+
+  scalar :snowflake, name: "Snowflake" do
+    serialize &Snowflake.dump/1
+    parse &parse_snowflake/1
+  end
+
+  defp parse_snowflake(%Absinthe.Blueprint.Input.String{value: value}) do
+    case Snowflake.cast!(value) do
+      {:ok, snowflake} -> {:ok, snowflake}
+      _error -> :error
+    end
+  end
+
+  defp parse_snowflake(%Absinthe.Blueprint.Input.Null{}) do
+    {:ok, nil}
+  end
+
+  defp parse_snowflake(_) do
+    :error
   end
 
   query do
+    @desc "Gets a list of messages in a channel."
     field :get_channel_messages, list_of(:message) do
+      @desc "The channel ID."
       arg :channel_id, non_null(:string)
+
+      @desc "How many messages should be fetched."
       arg :limit, :integer
 
       resolve fn _, %{channel_id: id, limit: limit}, _ ->
